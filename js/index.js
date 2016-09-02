a2aff03f9b1739aca7a88c5cf52552d80ed1f44d
chihaoyo <chihaoyo@gmail.com>
2016-09-02 19:31:24 +0800

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,38 @@
+var size = {w: 720, h: 300};
+var fill = d3.scaleOrdinal(d3.schemeCategory20);
+var layout = d3.layout.cloud()
+  .size([size.w, size.h])
+  .words(data.words)
+  .padding(4)
+  .rotate(function() { return ~~(Math.random()*2)*90; })
+  .fontSize(function(d) { return d.size*2; });
+var draw = function(words) {
+  d3.select("#main-word-cloud").append("svg")
+    .style('position', 'absolute')
+    .style('width', '100%')
+    .style('height', '100%')
+    .append('g')
+      .attr("transform", "translate(" + [layout.size()[0]/2, layout.size()[1]/2] + ")")
+      .selectAll("text")
+      .data(words)
+      .enter()
+      .append("text")
+        .style("font-size", function(d) { return d.size + "px"; })
+        .style("fill", function(d, i) { return fill(i); })
+        .attr("text-anchor", "middle")
+        .attr("transform", function(d) {
+          return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+        .text(function(d) { return d.text; });
+};
+$(function() {
+  $('#main-word-cloud').width(size.w).height(size.h);
+  layout.on('end', draw).start();
+
+  var $issue_list = $('#issues > ul');
+  var issues = data.issues.split('、');
+  for(var issue of issues) {
+    var $item = $('<li>').appendTo($issue_list);
+    $('<a>').attr('href', 'issue/' + issue).text(issue).appendTo($item);
+  }
+})
